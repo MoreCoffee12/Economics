287c0535129b4b2a93ad288b4ee46c383e7ea10c
Brian Howard <bhoward314159@gmail.com>
2025-08-28 06:41:51 -0700

    run_rmd standing up

--- run_rmd.bat
+++ run_rmd.bat
@@ -1,2 +1,3 @@
 rscript -e "rmarkdown::render('C:/Users/Rainy/Documents/GitHub/Economics/RecessionIndicators.Rmd')"
+rscript -e "rmarkdown::render('D:/Local Documents/GitHub/Economics/RecessionIndicators.Rmd')"
 pause
